92043657c52562a7b2fa71a5a9d90fe90b6e1986
Adi-T24 <adityast.thakur@gmail.com>
2024-05-09 23:13:12 -0400

    2ndcommit

--- app.py
+++ app.py
@@ -1,30 +1,68 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite://hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
-
+# Base.prepare(autoload_with = engine)
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
 # Save references to each table
-
-
+Measurement = Base.classes.measurement
+Station= Base.classes.station
 # Create our session (link) from Python to the DB
-
-
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt/&lt;end&gt"
+    )
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    start_date = datetime.strptime(most_recent_date, '%Y-%m-%d') - timedelta(days=365)
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= start_date).all()
+    session.close()
+precipitation_dict = {date: prcp for date, prcp in precipitation_data}
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    station_list = session.query(Station.station).all()
+    session.close()
+
+    return jsonify(station_list)
